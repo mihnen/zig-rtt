6ab034240bf9987529db9f322dc5eb11066d08f8
Matt Ihnen <matt.ihnen@gmail.com>
2024-11-05 12:37:26 -0600

    Add support for both down and up channels

--- src/root.zig
+++ src/root.zig
@@ -11,43 +11,72 @@ pub fn Rtt() type {
         const fmt = std.fmt;
         const Self = @This();
 
-        const NumOfChannels = config.up_channels;
-        channels: [NumOfChannels]Channel = undefined,
+        const NumOfUpChannels = config.up_channels;
+        const NumOfDownChannels = config.down_channels;
+        up_channels: [NumOfUpChannels]UpChannel = undefined,
+        down_channels: [NumOfDownChannels]DownChannel = undefined,
 
         pub fn print(self: *const Self, comptime fmt_str: []const u8, args: anytype) void {
-            const writer = Writer{ .chan = &self.channels[0] };
+            const writer = Writer{ .chan = &self.up_channels[0] };
             fmt.format(writer, fmt_str, args) catch unreachable;
         }
 
         pub fn init() Self {
-            comptime std.debug.assert(NumOfChannels > 0);
+            comptime std.debug.assert(NumOfUpChannels > 0 or NumOfDownChannels > 0);
             var self: @This() = .{};
-            for (&self.channels, 0..) |*chan, i| {
+            for (&self.up_channels, 0..) |*chan, i| {
+                chan.* = .{ .chan_num = i };
+            }
+            for (&self.down_channels, 0..) |*chan, i| {
                 chan.* = .{ .chan_num = i };
             }
             return self;
         }
 
-        pub fn channel(self: *const Self, comptime num: comptime_int) *const Channel {
-            comptime std.debug.assert(num < NumOfChannels);
-            return &self.channels[num];
+        pub fn upChannel(self: *const Self, comptime num: comptime_int) *const UpChannel {
+            comptime std.debug.assert(num < NumOfUpChannels);
+            return &self.up_channels[num];
+        }
+
+        pub fn downChannel(self: *const Self, comptime num: comptime_int) *const DownChannel {
+            comptime std.debug.assert(num < NumOfDownChannels);
+            return &self.down_channels[num];
         }
 
         pub fn rttInit(_: Self) void {
             capi.SEGGER_RTT_Init();
         }
 
-        const Channel = struct {
+        const DownChannel = struct {
+            pub const Error = error{};
+            pub const Reader = std.io.Reader(DownChannel, Error, read);
+
             chan_num: u32,
 
-            pub fn print(self: *const Channel, comptime fmt_str: []const u8, args: anytype) void {
+            fn read(self: DownChannel, buf: []u8) DownChannel.Error!usize {
+                var i: usize = 0;
+                while (i == 0) {
+                    i += capi.SEGGER_RTT_Read(self.chan_num, buf.ptr, buf.len);
+                }
+                return i;
+            }
+
+            pub fn reader(self: DownChannel) Reader {
+                return .{ .context = self };
+            }
+        };
+
+        const UpChannel = struct {
+            chan_num: u32,
+
+            pub fn print(self: *const UpChannel, comptime fmt_str: []const u8, args: anytype) void {
                 const writer = Writer{ .chan = self };
                 fmt.format(writer, fmt_str, args) catch unreachable;
             }
         };
 
         const Writer = struct {
-            chan: *const Channel,
+            chan: *const UpChannel,
 
             pub const Error = error{}; // infallible
 
